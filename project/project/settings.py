b95c4282eaf8754cea175664501de9e39c5e744c
jacklin92 <jacklin92xxx@gmail.com>
2024-04-17 14:15:38 +0800

    登入登出功能

--- project/project/settings.py
+++ project/project/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "food",
 ]
 
 MIDDLEWARE = [
@@ -119,7 +120,6 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
-
 ]
 
 # Default primary key field type
